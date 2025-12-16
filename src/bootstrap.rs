3c66b70569d812ab497f4d44a2e215336040572f
kawagami <joelai1988@gmail.com>
2025-12-16 08:53:15 +0800

    feat: update config logic

--- src/bootstrap.rs
+++ src/bootstrap.rs
@@ -2,17 +2,17 @@ use crate::{config::AppConfig, state::AppState};
 use color_eyre::eyre::Result;
 use reqwest::Client;
 use sqlx::postgres::PgPoolOptions;
-use std::{sync::Arc, time::Duration};
+use std::sync::Arc;
 
 pub async fn setup_app_state(config: &AppConfig) -> Result<Arc<AppState>> {
     let db = PgPoolOptions::new()
-        .max_connections(20)
+        .max_connections(config.db_max_connections)
         .connect(&config.database_url)
         .await?;
 
     sqlx::migrate!("./migrations").run(&db).await?;
 
-    let http_client = Client::builder().timeout(Duration::from_secs(10)).build()?;
+    let http_client = Client::builder().timeout(config.request_timeout).build()?;
 
     Ok(Arc::new(AppState { db, http_client }))
 }
--- src/config.rs
+++ src/config.rs
@@ -1,33 +1,38 @@
 use std::time::Duration;
 
-/// 應用程式配置
 #[derive(Debug, Clone)]
 pub struct AppConfig {
-    /// 服務器埠號
     pub port: u16,
-    /// 服務器主機地址
     pub host: String,
-    /// 請求超時時間
     pub request_timeout: Duration,
-    /// 請求超時時間
     pub database_url: String,
+    pub db_max_connections: u32,
 }
 
 impl Default for AppConfig {
     fn default() -> Self {
         Self {
-            port: 3000,
-            host: "0.0.0.0".to_string(),
-            request_timeout: Duration::from_secs(10),
-            // database_url: "postgresql://kawa:kawa@host.docker.internal:5432/kawa".to_string(),
+            port: std::env::var("APP_PORT")
+                .expect("Not Found APP_PORT")
+                .parse::<u16>()
+                .expect("APP_PORT value must be a valid u16 number"),
+            host: std::env::var("APP_HOST").expect("Not Found APP_HOST"),
+            request_timeout: Duration::from_secs(
+                std::env::var("REQUEST_TIMEOUT")
+                    .expect("Not Found REQUEST_TIMEOUT")
+                    .parse::<u64>()
+                    .expect("REQUEST_TIMEOUT value must be a valid u64 number"),
+            ),
             database_url: std::env::var("DATABASE_URL").expect("Not Found DATABASE_URL"),
+            db_max_connections: std::env::var("DB_MAX_CONNECTIONS")
+                .expect("Not Found DB_MAX_CONNECTIONS")
+                .parse::<u32>()
+                .expect("DB_MAX_CONNECTIONS value must be a valid u32 number"),
         }
     }
 }
 
-/// 載入應用程式配置
 pub fn load_config() -> AppConfig {
-    // 載入 .env 檔案
     dotenvy::dotenv().ok();
 
     AppConfig::default()
